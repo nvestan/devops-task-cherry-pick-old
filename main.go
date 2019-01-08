180139dcce132609d251b90bd784125b260f0416
Vasiliy I Ozerov <vasiliyozerov@gmail.com>
2019-01-09 02:43:21 +0300

    Formatted code

--- main.go
+++ main.go
@@ -1,13 +1,14 @@
 package main
 
 import (
-    "fmt"
-    "log"
-    "net/http"
+	"fmt"
+	"log"
+	"net/http"
 	"strconv"
 )
 
 func sendResponse(w http.ResponseWriter, r *http.Request) {
+<<<<<<< HEAD
   keys, ok := r.URL.Query()["a"]
   if !ok || len(keys[0]) < 1 {
       log.Println("Url Param 'a' is missing")
@@ -32,14 +33,46 @@ func sendResponse(w http.ResponseWriter, r *http.Request) {
   }
 
   fmt.Fprintf(w, "%d + %d = %d", a, b, sum(a,b))
+=======
+	keys, ok := r.URL.Query()["a"]
+	if !ok || len(keys[0]) < 1 {
+		log.Println("Url Param 'a' is missing")
+		return
+	}
+
+	a, err := strconv.Atoi(keys[0])
+
+	if err != nil {
+		return
+	}
+
+	keys, ok = r.URL.Query()["b"]
+	if !ok || len(keys[0]) < 1 {
+		log.Println("Url Param 'b' is missing")
+		return
+	}
+	b, err := strconv.Atoi(keys[0])
+	if err != nil {
+		log.Println("Bad number!")
+		return
+	}
+
+	fmt.Fprintf(w, "%d + %d = %d", a, b, sumValues(a, b))
+>>>>>>> bf38ea1... Formatted code
 }
 
 func main() {
-    http.HandleFunc("/", sendResponse)
-    log.Fatal(http.ListenAndServe(":7000", nil))
+	http.HandleFunc("/", sendResponse)
+	log.Fatal(http.ListenAndServe(":7000", nil))
 }
 
+<<<<<<< HEAD
 func sum (a, b int) int {
   t := b + a
   return t
+=======
+func sumValues(a, b int) int {
+	t := b + a
+	return t
+>>>>>>> bf38ea1... Formatted code
 }
